215ff329d90f01f071ab0f69e5ec85707ef927c5
DryDish <57399961+DryDish@users.noreply.github.com>
2022-03-17 18:26:09 +0100

    initialize app and all it's dependencies
    TODO - Add docker support for running and building

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express, { Application, Request } from "express";
+
+const app: Application = express();
+const port = process.env.APP_PORT || 9000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Connected successfully on port ${port}`);
+});
