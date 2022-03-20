9ff24a63ebd32845454a1dd1dc9acc15bbd87c24
DryDish <57399961+DryDish@users.noreply.github.com>
2022-03-21 00:07:05 +0100

    change ..with_logs.sh to return the correct status code on exit

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -7,7 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
   expect(1+2).toBe(3);
 });
 
-// // Failing test
-// test('adds 1 + 2 to equal 4 - FAILING', () => {
-//   expect(1+2).toBe(4);
-// });
+// Failing test
+test('adds 1 + 2 to equal 4', () => {
+  expect(1+2).toBe(4);
+});
