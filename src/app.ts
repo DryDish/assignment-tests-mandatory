5c129acb4cc6a8acd18f6eec48e4ba5f5323aa1d
asbrkilroy <asbr@kilroy.net>
2022-03-24 14:09:51 +0100

    working-ish

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express, { Application, Request } from "express";
-import mysql, { Connection, Query, QueryError } from "mysql2";
+//import mysql, { Connection, Query, QueryError } from "mysql2";
+//import { getAllPostalCodes } from "./service/PostalCode/PostalCode";
+//import { createConnection } from "mysql2";
 
 const app: Application = express();
 const port = process.env.APP_PORT || 9000;
@@ -8,7 +10,16 @@ const port = process.env.APP_PORT || 9000;
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+/*
+const connection = createConnection({
+  host: "127.0.0.1",
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: "addresses",
+});
 
+let x = getAllPostalCodes(connection);
+*/
 app.listen(port, () => {
   console.log(`Connected successfully on port ${port}`);
 });
--- src/service/PostalCode/PostalCode.test.ts
+++ src/service/PostalCode/PostalCode.test.ts
@@ -1,19 +1,27 @@
 import { getAllPostalCodes } from "./PostalCode";
-import mysql, { Connection, createPool, Pool } from "mysql2";
+import { createConnection } from "mysql2";
+
 
 describe("checks genrating postal code and city", () => {
-  let connection: Pool;
-  beforeEach(async () => {
-    connection = await createPool({
+  const connection = createConnection({
+    host: "localhost",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: "addresses",
+  });
+  test("gets all available postal code and cities", () => {
+    getAllPostalCodes(connection);
+    /*    expect(postalCodes.length).toBe(589);*/
+  });
+  /*let connection: Pool;
+  beforeEach(() => {
+    connection = createPool({
       host: "localhost",
       user: "root",
       password: "",
       database: "addresses",
     });
     console.log("Connected to database");
-  });
-  test("gets all available postal code and cities", () => {
-    getAllPostalCodes(connection);
-    /*    expect(postalCodes.length).toBe(589);*/
-  });
+  });*/
+  
 });
