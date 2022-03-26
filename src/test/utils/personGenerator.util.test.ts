6887732af2010362c08ff2a696178d3208d18023
CristiPV <cristi.valentin29@gmail.com>
2022-03-26 12:50:30 +0100

    add getRandomPersonData tests

--- src/test/utils/personGenerator.util.test.ts
+++ src/test/utils/personGenerator.util.test.ts
@@ -1,4 +1,7 @@
-import PersonGenerator, { PersonData } from "../../utils/personGenerator.util";
+import PersonGenerator, {
+  PersonData,
+  PersonFullData,
+} from "../../utils/personGenerator.util";
 
 describe("Person Generator", () => {
   const testDataAmount = 100;
@@ -167,23 +170,223 @@ describe("Person Generator", () => {
       }
     });
   });
+
+  describe("Method getRandomPersonData()", () => {
+    it("should only contain gender 'male' or 'female'", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        expect(outputArray[i].gender).toMatch(/^(male|female)$/);
+      }
+    });
+
+    it("should have a `fullName` made of `name` and `surname` separated by space", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        expect(outputArray[i].fullName.split(" ")).toHaveLength(3);
+      }
+    });
+
+    it("should have a `fullName` that only contains unicode letters", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        expect(outputArray[i].fullName).toMatch(/^([ -.\p{L}\p{M}*]+)$/gu);
+      }
+    });
+
+    it("should have a CPR with a length of exactly ten characters", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        expect(outputArray[i].CPR).toHaveLength(10);
+      }
+    });
+
+    it("should have a CPR that only contains characters from 0 to 9", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        expect(outputArray[i].CPR).toMatch(/^[0-9]*$/);
+      }
+    });
+
+    it("should have a CPR with the proper format: ddMMyyRRRR", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        const testDay = Number(outputArray[i].CPR.slice(0, 2));
+        const testMonth = Number(outputArray[i].CPR.slice(2, 4));
+        const testYear = Number(outputArray[i].CPR.slice(4, 6));
+        const testDate = new Date(testYear, testMonth - 1, testDay);
+
+        expect(testDay).toBe(testDate.getDate());
+        expect(testMonth).toBe(testDate.getMonth() + 1);
+        expect(testYear).toBe(testDate.getFullYear() % 100);
+      }
+    });
+
+    it("should have a birth date that matches the format: yyyy-MM-dd and only contains characters from 0 to 9, and -", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        expect(outputArray[i].dateOfBirth).toMatch(
+          /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/
+        );
+      }
+    });
+
+    it("should have a birth date with the length of exactly ten characters", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        expect(outputArray[i].dateOfBirth).toHaveLength(10);
+      }
+    });
+
+    it("should have a valid birth date", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        const splitDate = outputArray[i].dateOfBirth.split("-");
+        const testDay = Number(splitDate[2]);
+        const testMonth = Number(splitDate[1]);
+        const testYear = Number(splitDate[0]);
+        const testDate = new Date(testYear, testMonth - 1, testDay);
+
+        expect(testDay).toBe(testDate.getDate());
+        expect(testMonth).toBe(testDate.getMonth() + 1);
+        expect(testYear).toBe(testDate.getFullYear());
+      }
+    });
+
+    it("should have a CPR that corresponds to the birth date", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        const splitDateOfBirth = outputArray[i].dateOfBirth.split("-");
+        const dateOfBirthDay = Number(splitDateOfBirth[2]);
+        const dateOfBirthMonth = Number(splitDateOfBirth[1]);
+        const dateOfBirthYear = Number(splitDateOfBirth[0]);
+        const dateOfBirth = new Date(
+          dateOfBirthYear,
+          dateOfBirthMonth - 1,
+          dateOfBirthDay
+        );
+
+        const cprDay = Number(outputArray[i].CPR.slice(0, 2));
+        const cprMonth = Number(outputArray[i].CPR.slice(2, 4));
+        let cprYear = Number(outputArray[i].CPR.slice(4, 6));
+        // Setting the correct full year
+        if (cprYear >= 23) {
+            cprYear += 1900;
+        } else {
+            cprYear += 2000;
+        }
+        const cprDate = new Date(cprYear, cprMonth - 1, cprDay);
+
+        expect(cprDate).toStrictEqual(dateOfBirth);
+      }
+    });
+
+    it("should have a birth date with the length of exactly ten characters", () => {
+      // Given
+      const personGenerator = new PersonGenerator();
+      const outputArray: Array<PersonFullData> = [];
+      const genders: Array<string> = ['female', 'male'];
+
+      // When
+      for (let i = 0; i < testDataAmount; i++) {
+        outputArray.push(personGenerator.getRandomPersonData());
+      }
+
+      // Then
+      for (let i = 0; i < testDataAmount; i++) {
+        const cprLastDigit = Number(outputArray[i].CPR[outputArray[i].CPR.length - 1]);
+
+        expect(outputArray[i].gender).toEqual(genders[cprLastDigit % 2]);
+      }
+    });
+  });
 });
-
-/*
-  Test cases
-
-  getRandomPersonData()
-  make sure that gender is only `female` or `male` - with a large sample data.
-  make sure that fullName is SOMEHOW made of `firstName` `lastName` separated by space.
-  make sure that fullName does not contain numbers. basically make sure that we follow this regex: ^([A-zÀ-úñÑþÞăîĂÎțȚâÂșȘ ]+)$
-  make sure that the CPR has the proper length: 10
-  make sure that the date has the proper format: ddMMyyRRRR
-  make sure it only has numbers from 0 - 9
-  make sure that the date has the proper length: 10 
-  make sure that the date has the proper format: yyyy-MM-dd
-  make sure that the date is valid -> split date in 3 and form a date object with it.
-  make sure that the CPR is odd for male and even for females
-  make sure that the CPR corresponds to the date of birth
-  make sure it only has numbers from 0 - 9 and -
-
-  */
